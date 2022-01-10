6503c5c40412f4e193e791a92619c33f0f52e771
kien <thangconnguoita@gmail.com>
2022-01-10 15:17:50 +0700

    fix open file

--- main.cpp
+++ main.cpp
@@ -8,23 +8,27 @@
 using namespace std;
 
 int main() {
-    ofstream myfile;
-    myfile.open ("../cpu_temperature.txt");
+    for (int i = 0; i < 10; ++i) {
+        ofstream myfile;
+        myfile.open ("../cpu_temperature.txt", ios_base::app);
 
-    for (int i = 0; i < 30; ++i) {
-        FILE* fcputemp = fopen("/sys/class/thermal/thermal_zone5/temp","r");
-        int cputemp;
-        while(!feof(fcputemp)){
-            fscanf(fcputemp, "%d", &cputemp);
+        for (int i = 0; i < 60; ++i) {
+            FILE* fcputemp = fopen("/sys/class/thermal/thermal_zone5/temp","r");
+            int cputemp;
+            while(!feof(fcputemp)){
+                fscanf(fcputemp, "%d", &cputemp);
+            }
+            cputemp = cputemp/1000;
+            fclose(fcputemp);
+
+            time_t now = time(0);
+            tm *datetime = localtime(&now);
+
+            myfile << "Time: "<< 5+datetime->tm_hour << ":" << 30+datetime->tm_min << ":" << datetime->tm_sec << " - " << cputemp << " C\n";
+            sleep(120);
         }
-        cputemp = cputemp/1000;
-        fclose(fcputemp);
-
-        time_t now = time(0);
-        tm *datetime = localtime(&now);
-
-        myfile << "Time: "<< 5+datetime->tm_hour << ":" << 30+datetime->tm_min << ":" << datetime->tm_sec << " - " << cputemp << " C\n";
-        sleep(120);
+        myfile.close();
+        sleep(60);
     }
-    myfile.close();
+
 }
