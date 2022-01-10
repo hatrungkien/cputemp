77ee970de90ee0cd4a0f44cc8c917ea2642aaaf8
kien <thangconnguoita@gmail.com>
2022-01-10 15:33:24 +0700

    fix open file

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@
 using namespace std;
 
 int main() {
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 50; ++i) {
         ofstream myfile;
         myfile.open ("../cpu_temperature.txt", ios_base::app);
 
-        for (int i = 0; i < 60; ++i) {
+        for (int i = 0; i < 2; ++i) {
             FILE* fcputemp = fopen("/sys/class/thermal/thermal_zone5/temp","r");
             int cputemp;
             while(!feof(fcputemp)){
@@ -24,11 +24,10 @@ int main() {
             time_t now = time(0);
             tm *datetime = localtime(&now);
 
-            myfile << "Time: "<< 5+datetime->tm_hour << ":" << 30+datetime->tm_min << ":" << datetime->tm_sec << " - " << cputemp << " C\n";
-            sleep(120);
+            myfile << "Time: "<< datetime->tm_hour << ":" << datetime->tm_min << ":" << datetime->tm_sec << " - " << cputemp << " C\n";
+            sleep(2);
         }
         myfile.close();
-        sleep(60);
+        sleep(120);
     }
-
 }
